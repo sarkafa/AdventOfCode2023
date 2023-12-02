216c06860c0261d1a204ef3bb98bbe934245f6f2
Sarka Faloutova <sarka.faloutova@outlook.cz>
2023-12-02 21:37:21 +0100

    edit readmes

--- src/main/kotlin/day01/test.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    val line = "oneight"
-    val digitOrWordPattern = Regex("(?=(one|two|three|four|five|six|seven|eight|nine|\\d))")
-    val matches = digitOrWordPattern.findAll(line)
-    println(line)
-    println()
-    matches.forEach {println(it.value)}
-}
